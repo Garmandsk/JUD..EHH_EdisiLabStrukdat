b6867c3e1cdbcf38e16773e8d2c911e2f99ffdfc
Garmandsk <162258672+Garmandsk@users.noreply.github.com>
2024-12-09 20:29:41 +0700

    Semoga Selesai (#4)
    * Penambahan fitur total biaya jarak pengiriman dan detail transaksi
    
    * Selesai Part 1
    
    * Mengefisienkan File

--- StrukdatJUD..EHH.cxx
+++ StrukdatJUD..EHH.cxx
@@ -15,16 +15,12 @@ int main() {
       switch (opsi) {
             case 1:
                 system("cls");
-               
                 tampilkanProduk();
-          
                 cin.ignore();
                 cin.get();
                 break;
             case 2:
                 system("cls");
-                // Lihat keranjang
-                cout << "Produk dalam keranjang:\n";
                 lihatKeranjang();
                 cin.ignore();
                 cin.get();
@@ -32,7 +28,6 @@ int main() {
 
             case 3:
                 system("cls");
-                // Lakukan pembayaran
                 lakukanPembayaran();
                 cin.ignore();
                 cin.get();
@@ -40,7 +35,6 @@ int main() {
 
             case 4:
                 system("cls");
-                // Lihat riwayat pembayaran
                 lihatRiwayatPembayaran();
                 cin.ignore();
                 cin.get();
@@ -48,17 +42,12 @@ int main() {
 
             case 5:
                 system("cls");
-                // Lihat detail transaksi
-                int idTransaksi;
-                cout << "Masukkan ID transaksi yang ingin dilihat: ";
-                cin >> idTransaksi;
-                lihatDetailTransaksi(idTransaksi);
+                lihatDetailTransaksi();
                 cin.ignore();
                 cin.get();
                 break;
           
             case 6:
-                // Keluar
                 cout << "Terima kasih telah berbelanja di toko kami!\n";
                 return 0;
             default:
